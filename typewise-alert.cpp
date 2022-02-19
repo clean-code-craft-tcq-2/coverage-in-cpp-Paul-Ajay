381dca578b48813fd4f8cfa91a1f9edaa04578ed
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-02-19 23:45:24 +0530

    replace switch with map;

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,5 +1,7 @@
 #include "typewise-alert.h"
 #include <stdio.h>
+#include <map>
+#include <vector>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
@@ -11,25 +13,32 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
+std::map<CoolingType, std::vector<int>> temperature_limit_map {
+  {PASSIVE_COOLING, {0, 35}},
+  {HI_ACTIVE_COOLING, {0, 45}},
+  {MED_ACTIVE_COOLING, {0,40}}
+};
+
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
   int lowerLimit = 0;
   int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  // switch(coolingType) {
+  //   case PASSIVE_COOLING:
+  //     lowerLimit = 0;
+  //     upperLimit = 35;
+  //     break;
+  //   case HI_ACTIVE_COOLING:
+  //     lowerLimit = 0;
+  //     upperLimit = 45;
+  //     break;
+  //   case MED_ACTIVE_COOLING:
+  //     lowerLimit = 0;
+  //     upperLimit = 40;
+  //     break;
+  // }
+
+  return inferBreach(temperatureInC, temperature_limit_map[coolingType].at(0), temperature_limit_map[coolingType].at(1));
 }
 
 void checkAndAlert(
