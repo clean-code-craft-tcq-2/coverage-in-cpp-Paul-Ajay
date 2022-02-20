6b3a5552a5cd817e14f425b7cedf730ffb204c0b
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-02-20 19:38:54 +0530

    add test cases

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -22,23 +22,6 @@ std::map<CoolingType, std::vector<int>> temperature_limit_map {
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  // switch(coolingType) {
-  //   case PASSIVE_COOLING:
-  //     lowerLimit = 0;
-  //     upperLimit = 35;
-  //     break;
-  //   case HI_ACTIVE_COOLING:
-  //     lowerLimit = 0;
-  //     upperLimit = 45;
-  //     break;
-  //   case MED_ACTIVE_COOLING:
-  //     lowerLimit = 0;
-  //     upperLimit = 40;
-  //     break;
-  // }
-
   return inferBreach(temperatureInC, temperature_limit_map[coolingType].at(0), temperature_limit_map[coolingType].at(1));
 }
 
