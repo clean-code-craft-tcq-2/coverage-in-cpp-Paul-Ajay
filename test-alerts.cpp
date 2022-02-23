1b82346fae63dc613aac059a3bd7c1659f6237dc
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-02-23 12:10:40 +0530

    test

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,31 +14,31 @@ TEST_CASE("classify temperature breach according to cooling") {
   REQUIRE(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 20) == NORMAL);
 }
 
-// TEST_CASE("test interface class") {
-//   TargectSelector emailTarget(new Email());
+TEST_CASE("test interface class") {
+  TargectSelector emailTarget(new Email());
+  Email::setEmail("abc@mail.com");
+  REQUIRE(emailTarget.targetInterface(NORMAL) == "");
+  REQUIRE(emailTarget.targetInterface(TOO_LOW) == "To: abc@mail.com\nHi, the temperature is too low");
+  TargectSelector controllerTarget(new Controller());
+  REQUIRE(controllerTarget.targetInterface(NORMAL) == "feed : 0");
+  REQUIRE(controllerTarget.targetInterface(TOO_HIGH) == "feed : 2");
+}
+
+// TEST_CASE("test email class") {
+//   Email emailTarget;
 //   Email::setEmail("abc@mail.com");
-//   REQUIRE(emailTarget.targetInterface(NORMAL) == "");
-//   REQUIRE(emailTarget.targetInterface(TOO_LOW) == "To: abc@mail.com\nHi, the temperature is too low");
-//   TargectSelector controllerTarget(new Controller());
-//   REQUIRE(controllerTarget.targetInterface(NORMAL) == "feed : 0");
-//   REQUIRE(controllerTarget.targetInterface(TOO_HIGH) == "feed : 2");
+//   REQUIRE(Email::getEmail() == "abc@mail.com");
+//   REQUIRE(emailTarget.sendOutput(TOO_LOW) == "To: abc@mail.com\nHi, the temperature is too low");
+//   REQUIRE(emailTarget.sendOutput(TOO_HIGH) == "To: abc@mail.com\nHi, the temperature is too high");
+//   REQUIRE(emailTarget.sendOutput(NORMAL) == ""); 
 // }
 
-TEST_CASE("test email class") {
-  Email emailTarget;
-  Email::setEmail("abc@mail.com");
-  REQUIRE(Email::getEmail() == "abc@mail.com");
-  REQUIRE(emailTarget.sendOutput(TOO_LOW) == "To: abc@mail.com\nHi, the temperature is too low");
-  REQUIRE(emailTarget.sendOutput(TOO_HIGH) == "To: abc@mail.com\nHi, the temperature is too high");
-  REQUIRE(emailTarget.sendOutput(NORMAL) == ""); 
-}
-
-TEST_CASE("test controller class") {
-  Controller controllerTarget;
-  REQUIRE(controllerTarget.sendOutput(NORMAL) == "feed : 0");
-  REQUIRE(controllerTarget.sendOutput(TOO_HIGH) == "feed : 2");
-  REQUIRE(controllerTarget.sendOutput(TOO_LOW) == "feed : 1");  
-}
+// TEST_CASE("test controller class") {
+//   Controller controllerTarget;
+//   REQUIRE(controllerTarget.sendOutput(NORMAL) == "feed : 0");
+//   REQUIRE(controllerTarget.sendOutput(TOO_HIGH) == "feed : 2");
+//   REQUIRE(controllerTarget.sendOutput(TOO_LOW) == "feed : 1");  
+// }
 
 TEST_CASE("test cooling type validation functionality") {
   REQUIRE(validateCoolingType(HI_ACTIVE_COOLING) == true);
@@ -46,12 +46,12 @@ TEST_CASE("test cooling type validation functionality") {
   REQUIRE(validateCoolingType(PASSIVE_COOLING) == true);
   REQUIRE(validateCoolingType(INVALID) == false);
 }
-// TEST_CASE("test temperature check and alert functionality") {
-//   // TargectSelector controllerTarget(new Controller());
-//   BatteryCharacter battery;
-//   // battery.coolingType = PASSIVE_COOLING;
-//   // REQUIRE(checkAndAlert(controllerTarget, battery, 40, *consolePrint) == ALERTSEND);
-//   TargectSelector emailTarget(new Email());
-//   battery.coolingType = INVALID;
-//   REQUIRE(checkAndAlert(emailTarget, battery, 40, *consolePrint) == ALERTNOTREQUIRED);
-// }
+TEST_CASE("test temperature check and alert functionality") {
+  TargectSelector controllerTarget(new Controller());
+  BatteryCharacter battery;
+  battery.coolingType = PASSIVE_COOLING;
+  REQUIRE(checkAndAlert(controllerTarget, battery, 40, *consolePrint) == ALERTSEND);
+  TargectSelector emailTarget(new Email());
+  battery.coolingType = INVALID;
+  REQUIRE(checkAndAlert(emailTarget, battery, 40, *consolePrint) == ALERTNOTREQUIRED);
+}
